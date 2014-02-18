40959d769f19bf3b1ed65defccccbb7d6ff739c8
Steve Kim <ElSteve05@gmail.com>
2014-02-18 16:46:19 -0600

    Can select several different movies using a dialog box

--- VideoAudioStreamer/gen/com/example/videoaudiostreamer/R.java
+++ VideoAudioStreamer/gen/com/example/videoaudiostreamer/R.java
@@ -28,10 +28,11 @@ public final class R {
         public static final int stop=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080006;
         public static final int pause=0x7f080002;
         public static final int play=0x7f080001;
         public static final int restart=0x7f080003;
+        public static final int selectMovie=0x7f080005;
         public static final int stop=0x7f080004;
         public static final int videoView1=0x7f080000;
     }
@@ -45,6 +46,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int pick_movie=0x7f050003;
     }
     public static final class style {
         /** 
